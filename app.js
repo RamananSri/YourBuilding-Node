13f7c470e23184c1769a5cca07294a485d757cd6
RamananSri <1058515@ucn.dk>
2017-10-05 10:13:44 +0200

    hej

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express"); // Webserver framework
+const bodyParser = require("body-parser"); // Post request parsing af f.eks. forms fra user
+
+const app = express();
+
+app.get("/", function(req, res) {
+	res.send("Hello World!");
+});
+
+app.get("/Ramanan", function(req, res) {
+	res.send("Hello Ramanan");
+});
+
+// Query parameters - localhost:3000/book?id=3
+app.get("/book/", (req, res) => {
+	res.send("hej " + req.query.id);
+});
+
+// Return JSON
+app.get("/books", (req, res) => {
+	res.json({ a: 123, b: 123 });
+});
+
+// Body parsers (urlencoded + json)
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
+
+app.get("/api/project", jsonParser, (req, res) => {
+	res.json({});
+});
+
+app.post("/api/projects", urlEncodedParser, (req, res) => {
+	res.sendStatus(200);
+});
+
+app.listen(3000, function() {
+	console.log("Example app listening on port 3000!");
+});
