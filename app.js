4684424b2fd4716d27d85f8849da2a2f55255cac
RamananSri <1058515@ucn.dk>
2017-10-17 10:24:34 +0200

    small change app.js

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
 const express = require("express"); // Webserver framework
 // const bodyParser = require("body-parser"); // Post request parsing af f.eks. forms fra user
-const index = require("./routers/index");
-const users = require("./routers/userRoute");
+const index = require("./src/routes/index");
+const users = require("./src/routes/userRoute");
 
 const app = express();
 
 app.use("/", index);
 app.use("/users", users);
-
-// app.get("/Ramanan", function(req, res) {
-// 	res.send("Hello Ramanan");
-// });
-
-// // Query parameters - localhost:3000/book?id=3
-// app.get("/book/", (req, res) => {
-// 	res.send("hej " + req.query.id);
-// });
-
-// // Return JSON
-// app.get("/books", (req, res) => {
-// 	res.json({ a: 123, b: 123 });
-// });
-
-// // Body parsers (urlencoded + json)
-// const urlEncodedParser = bodyParser.urlencoded({ extended: false });
-// const jsonParser = bodyParser.json();
-
-// app.get("/api/project", jsonParser, (req, res) => {
-// 	res.json({});
-// });
-
-// app.post("/api/projects", urlEncodedParser, (req, res) => {
-// 	res.sendStatus(200);
-// });
 
 // ip -  "192.168.87.101"
 
