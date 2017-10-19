40e05a27424657ed8e58cfffc801d92502354772
RamananSri <1058515@ucn.dk>
2017-10-19 15:03:55 +0200

    small bug fix

--- app.js
+++ app.js
@@ -4,31 +4,27 @@ const mongoose = require("mongoose"); // DB framework
 const swaggerTools = require("swagger-tools");
 const yaml = require("yamljs");
 const swaggerDoc = yaml.load("YBAPI.yaml");
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+swaggerTools.initializeMiddleware(swaggerDoc, function(middleware) {
 	app.use(middleware.swaggerUi());
 });
 
-const homeRoute = require("./src/routes/homeRoute");
+const index = require("./src/routes/index");
 const users = require("./src/routes/userRoute");
-const api = require("./src/routes/apiRoute");
+const auth = require("./src/routes/authRoute");
 
 const app = express();
 
 mongoose.connect("mongodb://dat:dat@ds119685.mlab.com:19685/yourbuilding", {
 	useMongoClient: true
 });
-//yb.dk/
-app.use("/", homeRoute);
-//yb.dk/api
-app.use("/api", api);
 
-//app.use("/api/users", users);
-//yb.dk/api/users
+app.use("/", auth);
+app.use("/index", index);
+app.use("/users", users);
+app.set("secretkey", "Brian");
 
 // ip -  "192.168.87.101"
 
-app.listen(3000, function () {
+app.listen(3000, function() {
 	console.log("Example app listening on port 3000!");
 });
-
-
