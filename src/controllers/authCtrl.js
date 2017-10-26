fab3807c804979c1321a12e3bf5465db0bc2cbc3
RamananSri <1058515@ucn.dk>
2017-10-26 13:39:33 +0200

    moved token to header

--- src/controllers/authCtrl.js
+++ src/controllers/authCtrl.js
@@ -4,50 +4,50 @@ var secret = "this is the secret secret secret 12356";
 const bcrypt = require("bcryptjs");
 
 var login = (req, res) => {
-  userDB.findOne({ email: req.body.email }, (error, user) => {
-    if (!user) {
-      res.json({
-        success: false,
-        message: "Password or username not found"
-      });
-    } else {
-      var token = jwt.sign(JSON.stringify(user._id), secret);
-      bcrypt.compare(req.body.password, user.password, (err, result) => {
-        if (result) {
-          res.json({
-            success: true,
-            message: "Tillykke du er inde i vores fede system",
-            token: token,
-            user: user
-          });
-        } else {
-          res.json({
-            success: false,
-            message: "Du er ikke logget ind!!"
-          });
-        }
-      });
-    }
-  });
+	userDB.findOne({ email: req.body.email }, (error, user) => {
+		if (!user) {
+			res.json({
+				success: false,
+				message: "Password or username not found"
+			});
+		} else {
+			var token = jwt.sign(JSON.stringify(user._id), secret);
+			bcrypt.compare(req.body.password, user.password, (err, result) => {
+				if (result) {
+					res.json({
+						success: true,
+						message: "Tillykke du er inde i vores fede system",
+						token: token,
+						user: user
+					});
+				} else {
+					res.json({
+						success: false,
+						message: "Du er ikke logget ind!!"
+					});
+				}
+			});
+		}
+	});
 };
 
 var authenticate = (req, res, next) => {
-  var token = req.body.token;
-  if (token) {
-    jwt.verify(token, secret, (error, decoded) => {
-      if (error) {
-        return res.json({ succes: false, message: "invalid token" });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    return res.json({ succes: false, message: "get a token noob" });
-  }
+	var token = req.headers.token;
+	if (token) {
+		jwt.verify(token, secret, (error, decoded) => {
+			if (error) {
+				return res.json({ succes: false, message: "invalid token" });
+			} else {
+				req.decoded = decoded;
+				next();
+			}
+		});
+	} else {
+		return res.json({ succes: false, message: "get a token noob" });
+	}
 };
 
 module.exports = {
-  login,
-  authenticate
+	login,
+	authenticate
 };
