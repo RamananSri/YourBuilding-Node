cbed6b699f742c053a88e9013f084537d19aabb1
RamananSri <1058515@ucn.dk>
2017-12-06 00:00:14 +0100

    categories

--- src/controllers/questionCtrl.js
+++ src/controllers/questionCtrl.js
@@ -2,14 +2,24 @@ const questionDB = require("../models/question").question;
 var mainCategories = require("../models/question").mainCategories;
 var subCategories = require("../models/question").subCategories;
 
-// Get array of available subcategories
+// Get array of available categorie/subcategories (this code sucks)
 var getAllCategories = (req, res) => {
-	if (req.params.id == 1) {
+	if (req.params.id == "Main") {
 		return res.json(mainCategories);
 	}
-	if (req.params.id == 2) {
-		return res.json(subCategories);
+	if (req.params.id == mainCategories[0]) {
+		return res.json([subCategories[0], subCategories[1]]);
 	}
+	if (req.params.id == mainCategories[1]) {
+		return res.json([subCategories[2], subCategories[3]]);
+	}
+	if (req.params.id == mainCategories[2]) {
+		return res.json([subCategories[4], subCategories[5]]);
+	}
+	return res.json({
+		success: false,
+		message: "No categories found"
+	});
 };
 
 var getBySubCategory = (req, res) => {
