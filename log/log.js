72eff2f5cd7d7ba6d3982642bdfc4acea257f72a
Daniel070793 <daniel070793@gmail.com>
2017-12-06 11:13:50 +0100

    added 1 hour to logging time

--- log/log.js
+++ log/log.js
@@ -1,7 +1,8 @@
 var fs = require("fs");
 var moment = require("moment");
+moment().add(1, "hours");
 
-var logErrors = (path, message) => { fs.appendFile(path, moment().locale("dk").format('LLL') + " " + message + "\r\n") };
+var logErrors = (path, message) => { fs.appendFile(path, moment().locale("dk").format('LLL') + " Error: " + message + "\r\n") };
 
 module.exports = {
 	logErrors
