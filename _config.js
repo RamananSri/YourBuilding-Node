54f421a30fa37e9e69b333259603a0d4eee5692f
RamananSri <1058515@ucn.dk>
2017-12-06 09:30:13 +0100

    clean

--- _config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.mongoURI = {
-    development: "mongodb://dat:dat@ds119685.mlab.com:19685/yourbuilding",
-    test: "mongodb://dat:dat@ds111066.mlab.com:11066/testingdb"
-};
-
-module.exports = config;
--- app.js
+++ app.js
@@ -2,34 +2,22 @@
 const express = require("express");
 const app = express();
 
-var config = require("./_config");
-
 app.listen(3000, () => {
 	console.log("Example app listening on port 3000!");
 }); // ip -  "192.168.87.101"
 
-
 // MongoDB framework
 const mongoose = require("mongoose");
 
-mongoose.connect(config.mongoURI[app.settings.env], function (err, res) {
-	useMongoClient: true;
-	if (err) {
-		console.log("Error connecting to the database. " + err);
-	} else {
-		console.log("Connected to Database: " + config.mongoURI[app.settings.env]);
-	}
+mongoose.connect("mongodb://dat:dat@ds119685.mlab.com:19685/yourbuilding", {
+	useMongoClient: true
 });
-
-// mongoose.connect("mongodb://dat:dat@ds119685.mlab.com:19685/yourbuilding", {
-// 	useMongoClient: true
-// });
 
 // Swagger documentation
 const swaggerTools = require("swagger-tools");
 const yaml = require("yamljs");
 const swaggerDoc = yaml.load("YBAPI.yaml");
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+swaggerTools.initializeMiddleware(swaggerDoc, function(middleware) {
 	app.use(middleware.swaggerUi());
 });
 
