dcb87ae861cfaade69404f8b2c04a6c2d810b3df
Daniel070793 <daniel070793@gmail.com>
2017-12-06 11:15:54 +0100

    now +1 hour works

--- log/log.js
+++ log/log.js
@@ -1,8 +1,7 @@
 var fs = require("fs");
 var moment = require("moment");
-moment().add(1, "hours");
 
-var logErrors = (path, message) => { fs.appendFile(path, moment().locale("dk").format('LLL') + " Error: " + message + "\r\n") };
+var logErrors = (path, message) => { fs.appendFile(path, moment().locale("dk").format('LLL').add(1, "hours") + " Error: " + message + "\r\n") };
 
 module.exports = {
 	logErrors
