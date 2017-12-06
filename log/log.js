ef96d126e2b56118c3f3b528958f97fa0d453524
Daniel070793 <daniel070793@gmail.com>
2017-12-06 11:18:23 +0100

    works?

--- log/log.js
+++ log/log.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var moment = require("moment");
 
-var logErrors = (path, message) => { fs.appendFile(path, moment().locale("dk").format('LLL').add(1, "hours") + " Error: " + message + "\r\n") };
+var logErrors = (path, message) => { fs.appendFile(path, moment().add(1, "hours").locale("dk").format('LLL') + " Error: " + message + "\r\n") };
 
 module.exports = {
 	logErrors
